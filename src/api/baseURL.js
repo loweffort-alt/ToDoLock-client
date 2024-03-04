549c6a50242df7ec4d4cdf2b51f9e557992cc966
loweffort-alt <farfan_alexander@outlook.com>
2024-03-04 02:35:24 -0500

    Hotfix: BaseURL changed to production server

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,2 +1,2 @@
-//export const baseURL = "https://todolock-server.onrender.com/api";
-export const baseURL = "http://localhost:3000/api";
+export const baseURL = "https://todolock-server.onrender.com/api";
+//export const baseURL = "http://localhost:3000/api";
